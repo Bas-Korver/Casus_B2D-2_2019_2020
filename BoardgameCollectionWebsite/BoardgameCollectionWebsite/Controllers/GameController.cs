1d9f3ee2ac8e99d5e0e251cecbfb0a368790c581
ParrotLync <43622266+ParrotLync@users.noreply.github.com>
2020-01-27 11:55:27 +0100

    Update namespace

--- BoardgameCollectionWebsite/BoardgameCollectionWebsite/Controllers/GameController.cs
+++ BoardgameCollectionWebsite/BoardgameCollectionWebsite/Controllers/GameController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BoardGames.Controllers
+namespace BoardgameCollectionWebsite.Controllers
 {
     public class GameController : Controller
     {
