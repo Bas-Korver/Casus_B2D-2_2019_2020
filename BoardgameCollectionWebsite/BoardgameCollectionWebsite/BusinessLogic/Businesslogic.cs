a1426a7bf9ae4c6b0c8a264d171fca6f83c9a41a
Nick Rau <nickrau3@gmail.com>
2020-01-27 21:33:29 +0100

    Bij getBoardById de connecties toegevoegd. Moet nog getest worden en dan bij de rest ook gedaan worden

--- BoardgameCollectionWebsite/BoardgameCollectionWebsite/BusinessLogic/Businesslogic.cs
+++ BoardgameCollectionWebsite/BoardgameCollectionWebsite/BusinessLogic/Businesslogic.cs
@@ -85,6 +85,210 @@ namespace BoardgameCollectionWebsite.BusinessLogic
 
                     db.BoardGames.Add(foundBoard);
                     db.SaveChanges();
+                    foundBoard = (from bor in db.BoardGames where bor.BoardgameID == foundBoard.BoardgameID select bor).FirstOrDefault();
+
+                    List < Category > categories = new List<Category>();
+                    List<Designer> designers = new List<Designer>();
+                    List<Artist> artists = new List<Artist>();
+                    List<Publisher> publishers = new List<Publisher>();
+                    List<Family> families = new List<Family>();
+                    List<Expansion> expansions = new List<Expansion>();
+                    List<Mechanic> mechanics = new List<Mechanic>();
+                    List<Implementation> implementations = new List<Implementation>();
+
+                    foreach (Link link in bggBoard.Link)
+                    {
+                        if (link.Type == "boardgamecategory")
+                        {
+                            categories.Add(new Category() { CategoryID = Int32.Parse(link.Id), CategoryName = link.Value });
+                        }
+                        else if (link.Type == "boardgamedesigner")
+                        {
+                            designers.Add(new Designer() { DesignerID = Int32.Parse(link.Id), DesignerName = link.Value });
+                        }
+                        else if (link.Type == "boardgameartist")
+                        {
+                            artists.Add(new Artist() { ArtistID = Int32.Parse(link.Id), ArtistName = link.Value });
+                        }
+                        else if (link.Type == "boardgamepublisher")
+                        {
+                            publishers.Add(new Publisher() { PublisherID = Int32.Parse(link.Id), PublisherName = link.Value });
+                        }
+                        else if (link.Type == "boardgamefamily")
+                        {
+                            families.Add(new Family() { FamilyID = Int32.Parse(link.Id), FamilyName = link.Value });
+                        }
+                        else if (link.Type == "boardgameexpansion")
+                        {
+                            expansions.Add(new Expansion() { ExpansionID = Int32.Parse(link.Id), ExpansionName = link.Value });
+                        }
+                        else if (link.Type == "boardgamemechanic")
+                        {
+                            mechanics.Add(new Mechanic() { MechanicID = Int32.Parse(link.Id), MechanicName = link.Value });
+                        }
+                        else if (link.Type == "boardgameimplementation")
+                        {
+                            implementations.Add(new Implementation() { ImplementationID = Int32.Parse(link.Id), ImplementedBy = link.Value });
+                        }
+                    }
+                    
+                    if (categories.Count != 0)
+                    {
+                        foreach(Category cat in categories)
+                        {
+                            Category foundCategory = db.Categories.Find(cat.CategoryID);
+                            if(foundCategory == null)
+                            {
+                                List<BGGBoard> res = BGGConnector.Connector.GetBoardsByName(cat.CategoryName, "boardgamecategory");
+                                foundCategory = new Category() { CategoryID = Int32.Parse(res[0].Id), CategoryName = res[0].Name.ToString() };
+                                db.Categories.Add(foundCategory);
+                                db.SaveChanges();
+                                foundCategory = (from findCat in db.Categories where findCat.CategoryID == cat.CategoryID select findCat).FirstOrDefault();
+                            }
+
+                            BoardgameCategory boardgameCategory = new BoardgameCategory() { Category = foundCategory, BoardGame = foundBoard };
+                            db.BoardgameCategories.Add(boardgameCategory);
+                            db.SaveChanges();
+                        }
+                    }
+                    if (designers.Count != 0)
+                    {
+                        foreach (Designer des in designers)
+                        {
+                            Designer foundDesigner = db.Designers.Find(des.DesignerID);
+                            if (foundDesigner == null)
+                            {
+                                List<BGGBoard> res = BGGConnector.Connector.GetBoardsByName(des.DesignerName, "boardgamedesigner");
+                                foundDesigner = new Designer() { DesignerID = Int32.Parse(res[0].Id), DesignerName = res[0].Name.ToString() };
+                                db.Designers.Add(foundDesigner);
+                                db.SaveChanges();
+                                foundDesigner = (from findCat in db.Designers where findCat.DesignerID == des.DesignerID select findCat).FirstOrDefault();
+                            }
+
+                            BoardgameDesigner boardgameDesigner = new BoardgameDesigner() { Designer = foundDesigner, BoardGame = foundBoard };
+                            db.BoardgameDesigners.Add(boardgameDesigner);
+                            db.SaveChanges();
+                        }
+                    }
+
+                   
+                    if (artists.Count != 0)
+                    {
+                        foreach (Artist art in artists)
+                        {
+                            Artist foundArtist = db.Artists.Find(art.ArtistID);
+                            if (foundArtist == null)
+                            {
+                                List<BGGBoard> res = BGGConnector.Connector.GetBoardsByName(art.ArtistName, "boardgameartist");
+                                foundArtist = new Artist() { ArtistID = Int32.Parse(res[0].Id), ArtistName = res[0].Name.ToString() };
+                                db.Artists.Add(foundArtist);
+                                db.SaveChanges();
+                                foundArtist = (from findCat in db.Artists where findCat.ArtistID == art.ArtistID select findCat).FirstOrDefault();
+                            }
+
+                            BoardgameArtist boardgameArtist = new BoardgameArtist() { Artist = foundArtist, BoardGame = foundBoard };
+                            db.BoardgameArtists.Add(boardgameArtist);
+                            db.SaveChanges();
+                        }
+                    }
+                    if (publishers.Count != 0)
+                    {
+                        foreach (Publisher pub in publishers)
+                        {
+                            Publisher foundPublisher = db.Publishers.Find(pub.PublisherID);
+                            if (foundPublisher == null)
+                            {
+                                List<BGGBoard> res = BGGConnector.Connector.GetBoardsByName(pub.PublisherName, "boardgameartist");
+                                foundPublisher = new Publisher() { PublisherID = Int32.Parse(res[0].Id), PublisherName = res[0].Name.ToString() };
+                                db.Publishers.Add(foundPublisher);
+                                db.SaveChanges();
+                                foundPublisher = (from findCat in db.Publishers where findCat.PublisherID == pub.PublisherID select findCat).FirstOrDefault();
+                            }
+
+                            BoardgamePublisher boardgamePublisher = new BoardgamePublisher() { Publisher = foundPublisher, BoardGame = foundBoard };
+                            db.BoardgamePublishers.Add(boardgamePublisher);
+                            db.SaveChanges();
+                        }
+                    }
+                    // TODO: Voor de rest nog doen
+                    if (families.Count != 0)
+                    {
+                        foreach (Family fam in families)
+                        {
+                            Family foundFamily = db.Families.Find(fam.FamilyID);
+                            if (foundFamily == null)
+                            {
+                                List<BGGBoard> res = BGGConnector.Connector.GetBoardsByName(fam.FamilyName, "boardgamefamily");
+                                foundFamily = new Family() { FamilyID = Int32.Parse(res[0].Id), FamilyName = res[0].Name.ToString() };
+                                db.Families.Add(foundFamily);
+                                db.SaveChanges();
+                                foundFamily = (from findCat in db.Families where findCat.FamilyID == fam.FamilyID select findCat).FirstOrDefault();
+                            }
+
+                            BoardgameFamily boardgameFamily = new BoardgameFamily() { Family = foundFamily, BoardGame = foundBoard };
+                            db.BoardgameFamilies.Add(boardgameFamily);
+                            db.SaveChanges();
+                        }
+                    }
+                    if (expansions.Count != 0)
+                    {
+                        foreach (Expansion exp in expansions)
+                        {
+                            Expansion foundExpansion = db.Expansions.Find(exp.ExpansionID);
+                            if (foundExpansion == null)
+                            {
+                                List<BGGBoard> res = BGGConnector.Connector.GetBoardsByName(exp.ExpansionName, "boardgameexpansion");
+                                foundExpansion = new Expansion() { ExpansionID = Int32.Parse(res[0].Id), ExpansionName = res[0].Name.ToString() };
+                                db.Expansions.Add(foundExpansion);
+                                db.SaveChanges();
+                                foundExpansion = (from findCat in db.Expansions where findCat.ExpansionID == exp.ExpansionID select findCat).FirstOrDefault();
+                            }
+
+                            BoardgameExpansion boardgameExpansion = new BoardgameExpansion() { Expansion = foundExpansion, BoardGame = foundBoard };
+                            db.BoardgameExpansions.Add(boardgameExpansion);
+                            db.SaveChanges();
+                        }
+                    }
+                    if (mechanics.Count != 0)
+                    {
+                        foreach (Mechanic mech in mechanics)
+                        {
+                            Mechanic foundExpansion = db.Mechanics.Find(mech.MechanicID);
+                            if (foundExpansion == null)
+                            {
+                                List<BGGBoard> res = BGGConnector.Connector.GetBoardsByName(mech.MechanicName, "boardgameexpansion");
+                                foundExpansion = new Mechanic() { MechanicID = Int32.Parse(res[0].Id), MechanicName = res[0].Name.ToString() };
+                                db.Mechanics.Add(foundExpansion);
+                                db.SaveChanges();
+                                foundExpansion = (from findCat in db.Mechanics where findCat.MechanicID == mech.MechanicID select findCat).FirstOrDefault();
+                            }
+
+                            BoardgameMechanic boardgameMechanic = new BoardgameMechanic() { Mechanic = foundExpansion, BoardGame = foundBoard };
+                            db.BoardgameMechanics.Add(boardgameMechanic);
+                            db.SaveChanges();
+                        }
+                    }
+                    if (implementations.Count != 0)
+                    {
+                        foreach (Implementation imp in implementations)
+                        {
+                            Implementation foundImplementation = db.Implementations.Find(imp.ImplementationID);
+                            if (foundImplementation == null)
+                            {
+                                List<BGGBoard> res = BGGConnector.Connector.GetBoardsByName(imp.ImplementedBy, "boardgameexpansion");
+                                foundImplementation = new Implementation() { ImplementationID = Int32.Parse(res[0].Id), ImplementedBy = res[0].Name.ToString() };
+                                db.Implementations.Add(foundImplementation);
+                                db.SaveChanges();
+                                foundImplementation = (from findCat in db.Implementations where findCat.ImplementationID == imp.ImplementationID select findCat).FirstOrDefault();
+                            }
+
+                            BoardgameImplementation boardgameImplementation = new BoardgameImplementation() { Implementation = foundImplementation, BoardGame = foundBoard };
+                            db.BoardgameImplementations.Add(boardgameImplementation);
+                            db.SaveChanges();
+                        }
+                    }
+
+                    
                     return foundBoard;
                 }
             }
